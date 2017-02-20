34b5bec71a23709e168fdc9784dbe2be5901e5a8
Andrea Ciraolo <andrea.ciraolo@gmail.com>
2017-02-20 15:11:36 +0100

    Other attempt to fix Database.

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
 module.exports = {
-  url: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/ya_auth'
+  url: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ya_auth'
 }
